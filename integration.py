033f7b1b46d80eff00379fc7d9ce45e83338bdfd
Omar Aflak <aflakomar@gmail.com>
2019-09-08 23:46:40 +0100

    added comments

--- integration.py
+++ integration.py
@@ -9,14 +9,18 @@ function = lambda x : np.sin(x)
 xmin, xmax = 0, 2 * np.pi
 dx = 0.1
 
+# figure
 fig, ax = plt.subplots(figsize=(10, 4))
 
+# draw rectangles
 for x in np.arange(xmin, xmax, dx):
     ax.add_patch(Rectangle(xy=(x, 0), width=dx, height=function(x), color=(1, 0, 0, 0.2)))
 
+# draw function
 x = np.arange(xmin, xmax, 0.01)
 y = function(x)
-
-ax.grid(True)
 ax.plot(x, y)
+
+# show
+ax.grid(True)
 plt.show()
